a65eb297e98e9a41b17a93c49d69eff59a558183
Gabriel Ochsenhofer <gabs@gabs.tv>
2015-10-11 00:29:33 -0300

    get db stats

--- mgoplus.go
+++ mgoplus.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jmoiron/jsonq"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
-	"log"
 )
 
 // CollectionStats contains mostly size measurements (in bytes) of a collection.
@@ -63,6 +62,80 @@ type CollectionStats struct {
 	//ok int `bson:"ok"`
 }
 
+// The DbStats struct contains storage statistics for a given database.
+// All size values are in bytes.
+//
+// Members:
+//     DB
+//         Contains the name of the database.
+//     NCollections
+//         Contains a count of the number of collections in that database.
+//     NObjects
+//         Contains a count of the number of objects (i.e. documents) in
+//         the database across all collections.
+//     AvgObjSize
+//         The average size of each document in bytes. This is the dataSize
+//         divided by the number of documents.
+//     DataSize
+//         The total size in bytes of the data held in this database including
+//         the padding factor. The dataSize will not decrease when documents
+//         shrink, but will decrease when you remove documents.
+//     StorageSize
+//         The total amount of space in bytes allocated to collections in this
+//         database for document storage. The storageSize does not decrease as
+//         you remove or shrink documents.
+//     NumExtents
+//         Contains a count of the number of extents in the database across all collections.
+//     NIndexes
+//         Contains a count of the total number of indexes across all collections
+//         in the database.
+//     IndexSize
+//         The total size in bytes of all indexes created on this database.
+//     FileSize
+//         The total size in bytes of the data files that hold the database.
+//         This value includes preallocated space and the padding factor.
+//         The value of fileSize only reflects the size of the data files for
+//         the database and not the namespace file.
+//         Only present when using the mmapv1 storage engine.
+//     NsSizeMB
+//         The total size of the namespace files (i.e. that end with .ns) for
+//         this database. You cannot change the size of the namespace file
+//         after creating a database, but you can change the default size for
+//         all new namespace files with the nsSize runtime option.
+//         Only present when using the mmapv1 storage engine.
+//     DataFileVersion
+//         See DbStatsDataFileVersion
+//     ExtentFreeList
+//         See DbStatsExtentFreeList
+type DbStats struct {
+	DB              string                 `bson:"db"`
+	NCollections    int                    `bson:"collections"`
+	NObjects        int                    `bson:"objects"`
+	AvgObjSize      int64                  `bson:"avgObjSize"`
+	DataSize        int64                  `bson:"dataSize"`
+	StorageSize     int64                  `bson:"storageSize"`
+	NumExtents      int                    `bson:"numExtents"`
+	NIndexes        int                    `bson:"indexes"`
+	IndexSize       int64                  `bson:"indexSize"`
+	FileSize        int64                  `bson:"fileSize"`
+	NsSizeMB        int64                  `bson:"nsSizeMB"`
+	DataFileVersion DbStatsDataFileVersion `bson:"dataFileVersion"`
+	ExtentFreeList  DbStatsExtentFreeList  `bson:"extentFreeList"`
+}
+
+// Document that contains information about the on-disk format of the data files
+// for the database. Only present when using the mmapv1 storage engine.
+type DbStatsDataFileVersion struct {
+	Major int `bson:"major"`
+	Minor int `bson:"minor"`
+}
+
+// Only present when using the mmapv1 storage engine.
+type DbStatsExtentFreeList struct {
+	Num  int   `bson:"num"`
+	Size int64 `bson:"size"`
+}
+
 // Returns a slice containing the names of all collections in the current database.
 //
 // Based on
@@ -91,7 +164,6 @@ func GetCollectionNames(db *mgo.Database) ([]string, error) {
 // http://docs.mongodb.org/manual/reference/method/db.collection.stats/
 func GetCollectionStats(db *mgo.Database, collectionName string) (CollectionStats, error) {
 	var output CollectionStats
-	//raw := make(map[string]interface{})
 	err := db.Run(bson.D{
 		{"collStats", collectionName},
 		{"scale", 1},
@@ -101,6 +173,22 @@ func GetCollectionStats(db *mgo.Database, collectionName string) (CollectionStat
 	if err != nil {
 		return output, err
 	}
-	log.Println(output)
+	return output, nil
+}
+
+// Returns a variety of database statistics.
+//
+// Based on
+// http://docs.mongodb.org/manual/reference/command/dbStats/#dbcmd.dbStats
+func GetDbStats(db *mgo.Database) (DbStats, error) {
+	var output DbStats
+	err := db.Run(bson.D{
+		{"dbStats", 1},
+		{"scale", 1},
+	}, &output)
+
+	if err != nil {
+		return output, err
+	}
 	return output, nil
 }
